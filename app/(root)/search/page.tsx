078ddfecd17cde6bb1279064903309632e9a9608
DrDisagree <crazymahmud08@gmail.com>
2024-07-07 02:03:23 +0600

    Implement user search functionality

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,58 @@
+import UserCard from "@/components/cards/UserCard";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const userInfo = await fetchUser(user.id);
+
+  // Fetch all user
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+    sortBy: "desc",
+  });
+
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+      {/* TODO: Add search bar */}
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users found</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Page;
