deb40e6c5b2baca240a09971c7fc53c8421bd967
DrDisagree <crazymahmud08@gmail.com>
2024-07-06 14:56:34 +0600

    Add functions to update user profile

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,37 +1,42 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function Page() {
-    const user = await currentUser();
-    // TODO: fetch data from backend
-    const userInfo = {
-        _id: user?.id,
-        username: user?.username,
-        name: user?.firstName || "",
-        bio: "",
-        image: user?.imageUrl,
-    };
-    const userData = {
-        id: user?.id,
-        objectId: userInfo?._id,
-        username: userInfo?.username || user?.username,
-        name: userInfo?.name || user?.firstName || "",
-        bio: userInfo?.bio || "",
-        image: userInfo?.image || user?.imageUrl,
-    };
+  const user = await currentUser();
 
-    return (
-        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-            <h1 className="head-text">Onboarding</h1>
-            <p className="mt-3 text-base-regular text-light-2">
-                Complete your profile now to use Quirklr
-            </p>
-            <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile user={userData} btnTitle="Continue" />
-            </section>
-        </main>
-    )
+  if (!user) {
+    return null;
+  }
+
+  const userInfo = await fetchUser(user.id);
+
+  if (userInfo?.onboarded) {
+    redirect("/");
+  }
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Quirklr
+      </p>
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
+    </main>
+  );
 }
 
 export default Page;
--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidationSchema),
@@ -85,7 +89,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       }
     }
 
-    // TODO: update user profile
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+    if (pathname === "profile/edit") {
+      router.back();
+    } else {
+      router.push("/");
+    }
   };
 
   return (
