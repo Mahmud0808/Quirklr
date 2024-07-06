c2957834ecc6fc6fde72c8577d981711991214c7
DrDisagree <crazymahmud08@gmail.com>
2024-07-06 14:58:39 +0600

    Use react fragment

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,11 +3,11 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div>
+    <>
       <AnimatedBackground />
       <div className="flex h-screen items-center justify-center flex-col p-10">
         <SignIn forceRedirectUrl={"/onboarding"} />
       </div>
-    </div>
+    </>
   );
 }
