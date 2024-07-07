7e4c17eb680cf480db612bd208f712c63c474cf3
DrDisagree <crazymahmud08@gmail.com>
2024-07-07 20:35:30 +0600

    Expose clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/", "/api/webhook/clerk", "/onboarding"]);
+const isPublicRoute = createRouteMatcher(["/api/webhook/clerk"]);
 
 export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
+  if (isProtectedRoute(request) && !isPublicRoute(request)) {
     auth().protect();
   }
 });
