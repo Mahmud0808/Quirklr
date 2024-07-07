a564a8ac635e6f97cf8a1db3731418084dfb0d9e
DrDisagree <crazymahmud08@gmail.com>
2024-07-07 20:07:36 +0600

    Fix function arguments

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -201,12 +201,17 @@ export async function fetchThreadById(threadId: string) {
   }
 }
 
-export async function addCommentToThread(
-  threadId: string,
-  commentText: string,
-  userId: string,
-  path: string
-) {
+export async function addCommentToThread({
+  threadId,
+  commentText,
+  userId,
+  path,
+}: {
+  threadId: string;
+  commentText: string;
+  userId: string;
+  path: string;
+}) {
   connectToDB();
 
   try {
