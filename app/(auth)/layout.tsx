f6a16253c87b0626343f519d2298ac47882164d8
DrDisagree <crazymahmud08@gmail.com>
2024-07-10 00:19:24 +0600

    Show progress bar while loading pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,29 +1,33 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { dark } from "@clerk/themes";
+import NextTopLoader from "nextjs-toploader";
 import "../globals.css";
 
 export const metadata = {
-  title: "Quirklr",
-  description: "A Quirky Social Media Application",
+    title: "Quirklr",
+    description: "A Quirky Social Media Application",
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    <NextTopLoader showSpinner={false} />
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,40 +6,42 @@ import TopBar from "@/components/shared/TopBar";
 import LeftSideBar from "@/components/shared/LeftSideBar";
 import RightSideBar from "@/components/shared/RightSideBar";
 import BottomBar from "@/components/shared/BottomBar";
+import NextTopLoader from "nextjs-toploader";
 
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Quirklr",
-  description: "A Quirky Social Media Application",
+    title: "Quirklr",
+    description: "A Quirky Social Media Application",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
-      <html lang="en">
-        <body className={inter.className}>
-          <TopBar />
-          <main className="flex flex-row">
-            <LeftSideBar />
-            <section className="main-container">
-              <div className="w-full max-w-4xl">{children}</div>
-            </section>
-            <RightSideBar />
-          </main>
-          <BottomBar />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider
+            appearance={{
+                baseTheme: dark,
+            }}
+        >
+            <html lang="en">
+                <body className={inter.className}>
+                    <NextTopLoader showSpinner={false} />
+                    <TopBar />
+                    <main className="flex flex-row">
+                        <LeftSideBar />
+                        <section className="main-container">
+                            <div className="w-full max-w-4xl">{children}</div>
+                        </section>
+                        <RightSideBar />
+                    </main>
+                    <BottomBar />
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
